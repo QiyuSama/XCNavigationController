067d17be8d2127b3599c85b07eb07bccbfa9e463
xiangchao <455877644@qq.com>
2016-04-20 17:22:51 +0800

    Initial Commit

---
+++ XCNavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCNavigationController
+//
+//  Created by xiangchao on 16/4/20.
+//  Copyright © 2016年 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XCNavigationController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XCNavigationController
+//
+//  Created by xiangchao on 16/4/20.
+//  Copyright © 2016年 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
